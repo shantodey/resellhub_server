3accc56cba10c62329927d2f57c3d808cb491096
shantodey <ratul3330@gmail.com>
2026-06-24 21:38:07 +0600

    Implement MongoDB connection and product addition endpoint

--- index.js
+++ index.js
@@ -1,6 +1,67 @@
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const express = require('express');
+const dotenv = require('dotenv');
+const cors = require('cors');
+
+dotenv.config();
+
 const app = express();
-const port = 3000;
+const port = process.env.PORT;
+
+app.use(cors());
+app.use(express.json());
+
+
+
+
+
+const uri = process.env.MONGODB_URI;
+
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+
+    const db=client.db("ReSellHub");
+    const prodectCollection=db.collection('prodect');
+    const ordersCollection=db.collection('orders');
+    const reviewsCollection=db.collection('reviews');
+    const paymentsCollection=db.collection('payments');
+    
+
+    app.post("app/prodect",async(req,res)=>{
+        const{title,category,condition,price,images,description,sellerInfo}=req.body;
+        const addData={
+            title,category,condition,price,images,description,sellerInfo ,
+            createdAt:new Date(),
+            status:"active"
+        };
+        const result = await prodectCollection.insertOne(addData);
+        return result;
+    })
+
+
+
+
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+   
+  }
+}
+run().catch(console.dir);
+
+
+
+
+
+
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
